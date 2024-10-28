64f299c1d1a8cf5983f5b494a26d6baccbe854d4
Sak2212 <sashkoisaev5@gmail.com>
2024-10-28 14:12:31 +0600

    r

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 
@@ -7,7 +6,7 @@ def register(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')  # Перенаправление на страницу входа
+            return redirect('login')
     else:
         form = RegistrationForm()
     return render(request, 'accounts/register.html', {'form': form})
